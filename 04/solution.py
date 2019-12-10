fbe6bf2952a729b37eb923d55243f78a3fd1f8fb
Malik Mahmud <almalikmahmud@gmail.com>
2019-12-10 22:32:26 +0100

    Complete day 4 part 2 solution

--- 04/solution.py
+++ 04/solution.py
@@ -1,17 +1,27 @@
 def parseInput(input):
   return [int(x) for x in input.split('-')]
 
-def getPossiblePassswordCombinations(start, stop):
-  possibleCombinations = 0
+def possiblePassswordCombinations(start, stop):
+  possibleCombinations = []
   for number in range(start, stop):
     combination = str(number)
     if list(combination) == sorted(combination) and len(combination) != len(set(combination)):
-      possibleCombinations += 1
+      possibleCombinations.append(combination)
   
   return possibleCombinations
 
+def newPossiblePasswordCombinations(passwords):
+  possibleCombinations = 0
+  for password in passwords:
+    for digit in set(password):
+      if password.count(digit) == 2:
+        possibleCombinations += 1
+        break
+  
+  return possibleCombinations
 
 start, stop = parseInput('246540-787419')
-print(f"Possible Password Combinations Part 1: {getPossiblePassswordCombinations(start, stop)}")
-
+passwords = possiblePassswordCombinations(start, stop)
+print(f"Possible Password Combinations Part 1: {len(passwords)}")
+print(f"Possible Password Combinations Part 2: {newPossiblePasswordCombinations(passwords)}")
 
