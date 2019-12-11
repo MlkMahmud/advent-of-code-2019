ecaea586083ee00167c31700e3fb3897ff09908a
Malik Mahmud <almalikmahmud@gmail.com>
2019-12-11 20:10:01 +0100

    Change all funtion and variable names to snake_case

--- 02/solution.py
+++ 02/solution.py
@@ -1,40 +1,40 @@
-def convertInputToIntList(input):
+def convert_input_to_list(input):
   with open(input, "r") as file:
     return  [int(x) for x in file.read().strip().split(',')]
 
-def resetComputerMemory(input, noun, verb):
+def reset_computer_memory(input, noun, verb):
   program = list.copy(input)
   program[1] = noun
   program[2] = verb
   return program
   
-def restoreProgram(noun, verb):
-  program = resetComputerMemory(convertInputToIntList("02/input.txt"), noun, verb)
+def restore_program(noun, verb):
+  program = reset_computer_memory(convert_input_to_list("02/input.txt"), noun, verb)
   
   for i in range(0, len(program), 4):
     opcode = program[i]
     if opcode == 99:
       break
     else:
-      inputA = program[i + 1]
-      inputB = program[i + 2]
+      input_a = program[i + 1]
+      input_b = program[i + 2]
       output = program[i + 3]
       if opcode == 1:
-        program[output] = program[inputA] + program[inputB]
+        program[output] = program[input_a] + program[input_b]
       elif opcode == 2:
-        program[output] = program[inputA] * program[inputB]
+        program[output] = program[input_a] * program[input_b]
       else:
         continue
       
   return program[0]
 
-def findNounAndVerb(target):
+def find_noun_and_verb(target):
   for noun in range(100):
     for verb in range(100):
-      output = restoreProgram(noun, verb)
+      output = restore_program(noun, verb)
       if output == target:
         return f"Noun: {noun}, Verb: {verb}"
   return ""
 
-print(restoreProgram(12, 2))
-print(findNounAndVerb(19690720))
+print(restore_program(12, 2))
+print(find_noun_and_verb(19690720))
