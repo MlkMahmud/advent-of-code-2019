c21cce4a7e91fd825c90946b576f95d310ba9f8c
Malik Mahmud <almalikmahmud@gmail.com>
2019-12-15 20:12:11 +0100

    Complete day 6, part 1 challenge

---
+++ 06/solution.py
@@ -0,0 +1,24 @@
+def parse_input(input):
+  output = {}
+  with open(input) as file:
+    for line in file:
+      for a, b in [line.strip().split(')')]:
+        output[b] = a
+  return output
+
+
+def calculate_orbits(data):
+  orbits = 0
+  for key in data:
+    while key in data:
+      orbits += 1
+      key = data[key]
+  return orbits
+
+  
+  
+
+
+
+data = parse_input("06/input.txt")
+print(f"Part One: {calculate_orbits(data)}")
