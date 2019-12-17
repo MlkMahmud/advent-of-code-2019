777a1769f330eb874ca4d2ca669d2e1199f7e3f7
Malik Mahmud <almalikmahmud@gmail.com>
2019-12-17 22:57:09 +0100

    Complete day 8, part 2

--- 08/solution.py
+++ 08/solution.py
@@ -4,10 +4,10 @@ def parse_input(filename):
 
 
 def get_layers(image, width, height):
-  step = width * height
+  size = width * height
   layers = []
-  for i in range(0, len(image), step):
-    layers.append(image[i:i + step])
+  for i in range(0, len(image), size):
+    layers.append(image[i:i + size])
   return layers
 
 def layer_with_fewest_zeros(layers):
@@ -22,7 +22,28 @@ def layer_with_fewest_zeros(layers):
 def calculate(layer):
   return layer.count('1') * layer.count('2')
 
+def decode_image(layers):
+  decoded_image = layers[0]
+  for x in range(1, len(layers)):
+    layer = layers[x]
+    for i in range(len(layer)):
+      if decoded_image[i] == '2' and layer[i] in '10':
+        decoded_image[i] = layer[i]
+  return decoded_image
+
+
+def print_image(image, width):
+  for i in range(len(image)):
+    char = image[i]
+    if char == '0':
+      image[i] = " "
+    else:
+      image[i] = '#'
+  for x in range(0, len(image), width):
+    print("".join(image[x: x + width]))
+
 image = parse_input("input.txt")
 layers = get_layers(image, 25, 6)
 min_zeros = layer_with_fewest_zeros(layers)
 print(f"Part One: {calculate(min_zeros)}")
+print_image(decode_image(layers), 25)
